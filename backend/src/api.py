3e25da49e6f40164bb4cc145037180237e85ebe1
Manuel Alejandro Garisto Zuna <garistozunamanuel@gmail.com>
2020-12-12 18:48:28 -0400

    feat: Add get drinks api

--- backend/src/api.py
+++ backend/src/api.py
@@ -19,14 +19,15 @@ CORS(app)
 # db_drop_and_create_all()
 
 ## ROUTES
-'''
-@TODO implement endpoint
-    GET /drinks
-        it should be a public endpoint
-        it should contain only the drink.short() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
-'''
+@app.route('/drinks')
+def get_drinks():
+    drinks = Drink.query.all()
+    # If there's not drinks, abort(404).
+    if len(drinks) == 0:
+        abort(404)  # Not found.
+    # Using list comprehension to get a list of drinks short model representation.
+    list_of_drinks = [drink.short() for drink in drinks]
+    return jsonify({'success': True, 'drinks': list_of_drinks}), 200
 
 
 '''
